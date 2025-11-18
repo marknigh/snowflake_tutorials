05f1fcbe06d8dc5e2f048bfa8b3768d3c317bc60
Mark Nigh <marknigh70@gmail.com>
2025-11-18 16:53:46 -0600

    Update streamlist_app.py
    Add requests

--- streamlist_app.py
+++ streamlist_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Example Streamlit App :cup_with_straw:")
@@ -41,3 +42,6 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, $name_on_order ', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
